14eb6fd80c69a6544685afa96570be3e8e7ec693
sergiosl <sergioslcorreo@hotmail.com>
2017-11-30 17:04:31 +0100

    Inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'estaticas/home'
-
   get 'estaticas/index'
 
-  root 'home#index'
+  root 'estaticas#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
